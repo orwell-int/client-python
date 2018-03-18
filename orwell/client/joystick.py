78499b30dbe2ce2fa02e1b2ee23b111b225e0ae0
dchilot <dchilot@hotmail.com>
2018-03-18 15:49:38 +0000

    Try not to send input messages too often.

--- orwell/client/joystick.py
+++ orwell/client/joystick.py
@@ -24,12 +24,28 @@ class Joystick(object):
     ANGLE_MIN = 0.0
     ANGLE_MAX = math.pi * 0.5
 
-    def __init__(
-            self,
-            dead_zone,
+    JOYSTICKS = {}
+
+    @classmethod
+    def get_joystick(
+            cls,
             pygame_joystick,
+            dead_zone,
             angle=math.pi * 0.25,
             precision=0.025):
+        if (pygame_joystick.get_name() in cls.JOYSTICKS.keys()):
+            return cls.JOYSTICKS[pygame_joystick.get_name()]
+        else:
+            joystick = Joystick(pygame_joystick, dead_zone, angle, precision)
+            cls.JOYSTICKS[pygame_joystick.get_name()] = joystick
+            return joystick
+
+    def __init__(
+            self,
+            pygame_joystick,
+            dead_zone,
+            angle,
+            precision):
         assert(Joystick.ANGLE_MIN < angle < Joystick.ANGLE_MAX)
         self._dead_zone = dead_zone
         self._joystick_type = JoystickType.build(pygame_joystick.get_name())
@@ -43,6 +59,12 @@ class Joystick(object):
         self.start = False
         self._debug = False
         self._pygame_joystick = pygame_joystick
+        self._previous_left = None
+        self._previous_right = None
+        self._previous_fire_weapon1 = None
+        self._previous_fire_weapon2 = None
+        self._previous_start = False
+        self._has_new_values = False
 
     def _round(self, value):
         new_value = int(value / self._precision) * self._precision
@@ -56,15 +78,15 @@ class Joystick(object):
         y = self._pygame_joystick.get_axis(1)
         # print("y = " + str(y))
         if (self._debug):
-            if (buttons.get(2, 0) != 0):
+            if (self._pygame_joystick.get_button(2) != 0):
                 # X
                 self._angle -= 0.0001
                 print("angle = " + str(self._angle))
-            if (buttons.get(1, 0) != 0):
+            if (self._pygame_joystick.get_button(1) != 0):
                 # B
                 self._angle += 0.0001
                 print("angle = " + str(self._angle))
-            if (buttons.get(3, 0) != 0):
+            if (self._pygame_joystick.get_button(3) != 0):
                 # Y
                 self._toggle_direction()
         if (JoystickType.xinput == self._joystick_type):
@@ -72,10 +94,10 @@ class Joystick(object):
             factor = self._invert_direction * buttons.get(7, 0.0)
             # print("factor = " + str(factor))
             # left button (not arrow)
-            self.fire_weapon1 = (buttons.get(4, 0) != 0)
+            self.fire_weapon1 = (self._pygame_joystick.get_button(4) != 0)
             # left trigger
-            self.fire_weapon2 = (buttons.get(6, 0) != 0)
-            if (buttons.get(9, 0) != 0):
+            self.fire_weapon2 = (self._pygame_joystick.get_button(6) != 0)
+            if (self._pygame_joystick.get_button(9) != 0):
                 self.start = True
         else:
             # HOTAS
@@ -86,19 +108,20 @@ class Joystick(object):
             if (self._pygame_joystick.get_button(11) != 0):
                 self.start = True
         self._convert(x, y, factor)
+        if ((self._previous_left != self.left) or
+                (self._previous_right != self.right) or
+                (self._previous_fire_weapon1 != self.fire_weapon1) or
+                (self._previous_fire_weapon2 != self.fire_weapon2) or
+                (self._previous_start != self.start)):
+            self._has_new_values = True
+        self._previous_left = self.left
+        self._previous_right = self.right
+        self._previous_fire_weapon1 = self.fire_weapon1
+        self._previous_fire_weapon2 = self.fire_weapon2
+        self._previous_start = self.start
 
     def _toggle_direction(self):
         self._invert_direction = -self._invert_direction
-
-    def _convert2(self, x, y, factor):
-        cosine = math.cos(self._angle)
-        sine = math.sin(self._angle)
-        self.left = self._round(factor * (
-                x * cosine +
-                y * sine))
-        self.right = self._round(factor * (
-                y * cosine -
-                x * sine))
 
     def _convert(self, x, y, factor):
         cosine = math.cos(self._angle)
@@ -120,3 +143,7 @@ class Joystick(object):
         self.right = max(-1, min(
                 1,
                 big_right))
+
+    @property
+    def has_new_values(self):
+        return self._has_new_values
--- orwell/client/main.py
+++ orwell/client/main.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import exceptions
 import random
 import sys
+import logging
 
 import pygame
 import signal
@@ -215,14 +216,15 @@ class Toto(object):
         self._abort = True
 
     def send_input(self, joystick):
-        pb_input = pb_controller.Input()
-        pb_input.move.left = joystick.left
-        pb_input.move.right = joystick.right
-        pb_input.fire.weapon1 = joystick.fire_weapon1
-        pb_input.fire.weapon2 = joystick.fire_weapon2
-        payload = pb_input.SerializeToString()
-        message = self._routing_id + ' Input ' + payload
-        self._push_socket.send(message)
+        if (joystick.has_new_values):
+            pb_input = pb_controller.Input()
+            pb_input.move.left = joystick.left
+            pb_input.move.right = joystick.right
+            pb_input.fire.weapon1 = joystick.fire_weapon1
+            pb_input.fire.weapon2 = joystick.fire_weapon2
+            payload = pb_input.SerializeToString()
+            message = self._routing_id + ' Input ' + payload
+            self._push_socket.send(message)
 
 
 global TOTO
@@ -277,21 +279,21 @@ def main():
         k += 1
         for event in pygame.event.get(0.01):
             if event.type == pygame.JOYBUTTONDOWN:
-                print("Joystick button pressed.")
+                logging.debug("Joystick button pressed.")
             if event.type == pygame.JOYBUTTONUP:
-                print("Joystick button released.")
-            elif event.type == KEYDOWN:
-                print("Key down:", event.key)
-                if event.key == K_ESCAPE:
+                logging.debug("Joystick button released.")
+            elif event.type == pygame.KEYDOWN:
+                logging.debug("Key down:", event.key)
+                if event.key == pygame.K_ESCAPE:
                     done = True
         joystick_count = pygame.joystick.get_count()
         if (joystick_count > 1):
-            print("Warning,", joystick_count, " joysticks detected")
+            logging.warning("Warning,", joystick_count, " joysticks detected")
         for i in range(joystick_count):
             joystick = pygame.joystick.Joystick(i)
             if not joystick.get_init():
                 joystick.init()
-            joystick_wrapper = Joystick(sensivity, joystick)
+            joystick_wrapper = Joystick.get_joystick(joystick, sensivity)
             joystick_wrapper.process()
             # print("left =", joystick_wrapper.left, "; right =", joystick_wrapper.right)
             #print(joystick_wrapper.right)
@@ -310,5 +312,12 @@ def signal_handler(signal, frame):
     sys.exit(0)
 
 if ("__main__" == __name__):
+    logger = logging.getLogger()
+    handler = logging.StreamHandler()
+    formatter = logging.Formatter(
+            '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    logger.setLevel(logging.DEBUG)
     signal.signal(signal.SIGINT, signal_handler)
     main()
