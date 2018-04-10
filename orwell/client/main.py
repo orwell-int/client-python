08e227b06aeb263b0918c66e9060591e3eb8fa03
dchilot <dchilot@hotmail.com>
2018-04-10 11:28:30 +0000

    Reorganise classes.

--- orwell/client/main.py
+++ orwell/client/main.py
@@ -1,286 +1,30 @@
 from __future__ import print_function
-import random
-import os
-import sys
 import logging
-import time
+import os
+import random
+import sys
 
 import pygame
 import signal
 
-import zmq
-
-import orwell.messages.controller_pb2 as pb_controller
-import orwell.messages.server_game_pb2 as pb_server_game
-import orwell.messages.robot_pb2 as pb_robot
-from orwell.client.broadcast import Broadcast
 from orwell.client.joystick import Joystick
+from orwell.client.runner import Runner
 
 global NAME
 NAME = "client"
 
 
-class MessageWrapper(object):
-    def __init__(self, message):
-        recipient, message_type, payload = message.split(' ', 2)
-        self._recipient = recipient
-        self._message_type = message_type
-        self._payload = payload
-
-    @property
-    def recipient(self):
-        return self._recipient
-
-    @property
-    def message_type(self):
-        return self._message_type
-
-    @property
-    def payload(self):
-        return self._payload
-
-    def __str__(self):
-        return "(message, recipient = " + self._recipient + \
-            "; message_type = " + self._message_type + ")"
-
-
-class Toto(object):
-    STATE_INIT = "STATE_INIT"
-    STATE_HELLO_SENT = "STATE_HELLO_SENT"
-    STATE_WELCOME = "STATE_WELCOME"
-    STATE_HELLO_SENT_READY = "STATE_HELLO_SENT_READY"
-    STATE_WAITING_GAME_START = "STATE_WAITING_GAME_START"
-    STATE_GAME_RUNNING = "STATE_GAME_RUNNING"
-
-    def __init__(self, push_address=None, subscribe_address=None):
-        if ((push_address is None) or (subscribe_address is None)):
-            broadcast = Broadcast()
-            logging.info(broadcast.push_address +
-                    " / " + broadcast.subscribe_address)
-            self._push_address = broadcast.push_address
-            self._subscribe_address = broadcast.subscribe_address
-        else:
-            self._push_address = push_address
-            self._subscribe_address = subscribe_address
-        self._context = zmq.Context.instance()
-        self._push_socket = self._context.socket(zmq.PUSH)
-        self._push_socket.setsockopt(zmq.LINGER, 0)
-        self._push_socket.connect(self._push_address)
-        self._subscribe_socket = self._context.socket(zmq.SUB)
-        self._subscribe_socket.setsockopt(zmq.LINGER, 0)
-        self._subscribe_socket.connect(self._subscribe_address)
-        self._subscribe_socket.setsockopt(zmq.SUBSCRIBE, "")
-        self._routing_id = "temporary_id_" + str(random.randint(0, 32768))
-        self._robot = None
-        self._team = None
-        self._abort = False
-        self._state = Toto.STATE_INIT
-
-    def destroy(self):
-        self._push_socket.disconnect(self._push_address)
-        self._push_socket.close()
-        self._subscribe_socket.disconnect(self._subscribe_address)
-        self._subscribe_socket.close()
-        self._context.destroy()
-
-    def start(self):
-        assert(Toto.STATE_INIT == self._state)
-        hello = self._build_hello(True)
-        logging.info("send hello: " + repr(hello))
-        self._push_socket.send(hello)
-        self._state = Toto.STATE_HELLO_SENT
-
-    def process(self):
-        message_wrapper = self._receive()
-        if (message_wrapper is None):
-            return
-        logging.debug(self._state + " | " + str(message_wrapper))
-        if (("Pong" == message_wrapper.message_type) and
-                (self._routing_id == message_wrapper.recipient)):
-            self._decode_pong(message_wrapper)
-        if (Toto.STATE_HELLO_SENT == self._state):
-            if (self._routing_id == message_wrapper.recipient):
-                self._decode_hello_reply(message_wrapper)
-        elif (Toto.STATE_WELCOME == self._state):
-            self._decode_game_state_init(message_wrapper)
-        elif (Toto.STATE_HELLO_SENT_READY == self._state):
-            self._decode_hello_reply_ready(message_wrapper)
-        elif (Toto.STATE_WAITING_GAME_START):
-            self._decode_game_state_start(message_wrapper)
-        elif (Toto.STATE_GAME_RUNNING):
-            self._decode_game_state_running(message_wrapper)
-
-    def _receive(self):
-        try:
-            #message = self._subscribe_socket.recv()
-            message = self._subscribe_socket.recv(zmq.NOBLOCK)
-            if (message):
-                message_wrapper = MessageWrapper(message)
-                if (message_wrapper.recipient in
-                        ("all_clients", self._routing_id)):
-                    return message_wrapper
-        except zmq.Again as e:
-            # logging.debug("no message: " + str(e))
-            pass
-        return None
-
-    def _build_hello(self, ready):
-        pb_message = pb_controller.Hello()
-        name = "JAMBON"
-        pb_message.name = name
-        pb_message.ready = ready
-        payload = pb_message.SerializeToString()
-        return self._routing_id + ' Hello ' + payload
-
-    def _decode_pong(self, message_wrapper):
-        logging.debug("_decode_pong")
-        message = pb_robot.Pong()
-        message.ParseFromString(message_wrapper.payload)
-        logging.info(
-                "Pong ; len(timing) = " + str(len(message.timing)))
-        global NAME
-        for timing in message.timing:
-            timestamp = timing.timestamp
-            if (NAME == timing.logger):
-                timestamp_now = int(round(time.time() * 1000))
-                logging.info("timestamp_now = " + str(timestamp_now))
-                logging.info("timestamp = " + str(timestamp))
-                elapsed =  timestamp_now - timestamp
-            else:
-                elapsed = timing.elapsed
-            logging.info("'{logger}': @{timestamp} for {elapsed}".format(
-                logger=timing.logger,
-                timestamp=timestamp,
-                elapsed=elapsed))
-
-    def _decode_hello_reply(self, message_wrapper):
-        logging.debug("_decode_hello_reply " + str(message_wrapper.message_type))
-        if ("Welcome" == message_wrapper.message_type):
-            self._handle_welcome(message_wrapper.payload)
-        elif ("Goodbye" == message_wrapper.message_type):
-            self._handle_goodbye(message_wrapper.payload)
-        else:
-            logging.debug("Wrong message type: " + message_wrapper.message_type)
-
-    def _decode_hello_reply_ready(self, message_wrapper):
-        if ("Welcome" == message_wrapper.message_type):
-            self._handle_welcome_ready(message_wrapper.payload)
-        elif ("Goodbye" == message_wrapper.message_type):
-            self._handle_goodbye(message_wrapper.payload)
-        else:
-            logging.debug("Wrong message type: " + message_wrapper.message_type)
-
-    def _handle_welcome(self, payload):
-        message = pb_server_game.Welcome()
-        message.ParseFromString(payload)
-        logging.info(
-                "Welcome ; id = " + str(message.id) +
-                " ; robot = '" + message.robot + "'" +
-                " ; team = '" + message.team + "'")
-        self._robot = message.robot
-        self._team = message.team
-        self._routing_id = str(message.id)
-        self._state = Toto.STATE_WELCOME
-        if (message.game_state):
-            self._check_start_game(message.game_state)
-
-    def _handle_welcome_ready(self, payload):
-        message = pb_server_game.Welcome()
-        message.ParseFromString(payload)
-        logging.info(
-                "Welcome [ready] ; id = " + str(message.id) +
-                " ; robot = '" + message.robot + "'" +
-                " ; team = '" + message.team + "'")
-        self._robot = message.robot
-        self._team = message.team
-        self._routing_id = str(message.id)
-        if (message.game_state):
-            self._check_start_game(message.game_state)
-        else:
-            self._state = Toto.STATE_WAITING_GAME_START
-
-    def _configure(self, game_state):
-        logging.info("playing ? " + str(game_state.playing))
-        logging.info("time left: " + str(game_state.seconds))
-        # self._update_running(game_state.playing)
-        for team in game_state.teams:
-            logging.info(team.name + " (" + str(team.num_players) +
-                        ") -> " + str(team.score))
-        # let's assume we configure the different visualisations now
-        self._push_socket.send(self._build_hello(True))
-        self._state = Toto.STATE_HELLO_SENT_READY
-
-    def _check_start_game(self, game_state):
-        logging.info("_check_start_game: " + str(game_state.playing))
-        if (game_state.playing):
-            self._state = Toto.STATE_GAME_RUNNING
-        else:
-            self._state = Toto.STATE_WAITING_GAME_START
-
-    def _decode_game_state_init(self, message_wrapper):
-        if ("GameState" == message_wrapper.message_type):
-            message = pb_server_game.GameState()
-            message.ParseFromString(message_wrapper.payload)
-            self._configure(message)
-
-    def _decode_game_state_start(self, message_wrapper):
-        if ("GameState" == message_wrapper.message_type):
-            message = pb_server_game.GameState()
-            message.ParseFromString(message_wrapper.payload)
-            self._check_start_game(message)
-
-    def _decode_game_state_running(self, message_wrapper):
-        if ("GameState" == message_wrapper.message_type):
-            message = pb_server_game.GameState()
-            message.ParseFromString(message_wrapper.payload)
-            self._update_visualisations(message)
-
-    def _update_visualisations(self, game_state):
-        logging.info("Updating visualisations")
-        if (not game_state.running):
-            self._state = Toto.STATE_WAITING_GAME_START
-
-    def _handle_goodbye(self, payload):
-        message = pb_server_game.Goodbye()
-        message.ParseFromString(payload)
-        logging.info("Goodbye ...")
-        self._abort = True
-
-    def send_input(self, joystick, force_ping):
-        if (joystick.has_new_values):
-            pb_input = pb_controller.Input()
-            pb_input.move.left = joystick.left
-            pb_input.move.right = joystick.right
-            pb_input.fire.weapon1 = joystick.fire_weapon1
-            pb_input.fire.weapon2 = joystick.fire_weapon2
-            payload = pb_input.SerializeToString()
-            message = self._routing_id + ' Input ' + payload
-            logging.debug("message sent: " + repr(message))
-            self._push_socket.send(message)
-            if (joystick.ping or force_ping):
-                pb_ping = pb_controller.Ping()
-                timing_event = pb_ping.timing.add()
-                global NAME
-                timing_event.logger = NAME
-                timestamp = int(round(time.time() * 1000))
-                timing_event.timestamp = timestamp
-                payload = pb_ping.SerializeToString()
-                message = self._routing_id + ' Ping ' + payload
-                logging.info("message sent: " + repr(message))
-                self._push_socket.send(message)
-
-
-global TOTO
-TOTO = None
+global RUNNER
+RUNNER = None
 
 
 def main():
     random.seed(None)
-    #toto = Toto("tcp://192.168.1.11:9001", "tcp://192.168.1.11:9000")
-    toto = Toto()
-    global TOTO
-    TOTO = toto
-    toto.start()
+    #runner = Runner("tcp://192.168.1.11:9001", "tcp://192.168.1.11:9000")
+    runner = Runner()
+    global RUNNER
+    RUNNER = runner
+    runner.start()
     done = False
     pygame.init()
     os.putenv('SDL_VIDEODRIVER', 'dummy')
@@ -323,16 +67,16 @@ def main():
         #print(joystick_wrapper.right)
         #print(joystick_wrapper.fire_weapon1)
         #print(joystick_wrapper.fire_weapon2)
-        toto.send_input(joystick_wrapper, force_ping)
+        runner.send_input(joystick_wrapper, force_ping)
 
-        toto.process()
+        runner.process()
     pygame.quit()
 
 
 def signal_handler(signal, frame):
     logging.info('You pressed Ctrl+C!')
-    global TOTO
-    TOTO.destroy()
+    global RUNNER
+    RUNNER.destroy()
     pygame.quit()
     sys.exit(0)
 
---
+++ orwell/client/runner.py
@@ -0,0 +1,241 @@
+import logging
+import random
+import time
+
+import zmq
+
+import orwell.messages.controller_pb2 as pb_controller
+import orwell.messages.server_game_pb2 as pb_server_game
+import orwell.messages.robot_pb2 as pb_robot
+
+from orwell.client.broadcast import Broadcast
+from orwell.client.message_wrapper import MessageWrapper
+
+
+class Runner(object):
+    STATE_INIT = "STATE_INIT"
+    STATE_HELLO_SENT = "STATE_HELLO_SENT"
+    STATE_WELCOME = "STATE_WELCOME"
+    STATE_HELLO_SENT_READY = "STATE_HELLO_SENT_READY"
+    STATE_WAITING_GAME_START = "STATE_WAITING_GAME_START"
+    STATE_GAME_RUNNING = "STATE_GAME_RUNNING"
+
+    def __init__(self, push_address=None, subscribe_address=None):
+        if ((push_address is None) or (subscribe_address is None)):
+            broadcast = Broadcast()
+            logging.info(broadcast.push_address +
+                         " / " + broadcast.subscribe_address)
+            self._push_address = broadcast.push_address
+            self._subscribe_address = broadcast.subscribe_address
+        else:
+            self._push_address = push_address
+            self._subscribe_address = subscribe_address
+        self._context = zmq.Context.instance()
+        self._push_socket = self._context.socket(zmq.PUSH)
+        self._push_socket.setsockopt(zmq.LINGER, 0)
+        self._push_socket.connect(self._push_address)
+        self._subscribe_socket = self._context.socket(zmq.SUB)
+        self._subscribe_socket.setsockopt(zmq.LINGER, 0)
+        self._subscribe_socket.connect(self._subscribe_address)
+        self._subscribe_socket.setsockopt(zmq.SUBSCRIBE, "")
+        self._routing_id = "temporary_id_" + str(random.randint(0, 32768))
+        self._robot = None
+        self._team = None
+        self._abort = False
+        self._state = Runner.STATE_INIT
+
+    def destroy(self):
+        self._push_socket.disconnect(self._push_address)
+        self._push_socket.close()
+        self._subscribe_socket.disconnect(self._subscribe_address)
+        self._subscribe_socket.close()
+        self._context.destroy()
+
+    def start(self):
+        assert(Runner.STATE_INIT == self._state)
+        hello = self._build_hello(True)
+        logging.info("send hello: " + repr(hello))
+        self._push_socket.send(hello)
+        self._state = Runner.STATE_HELLO_SENT
+
+    def process(self):
+        message_wrapper = self._receive()
+        if (message_wrapper is None):
+            return
+        logging.debug(self._state + " | " + str(message_wrapper))
+        if (("Pong" == message_wrapper.message_type) and
+                (self._routing_id == message_wrapper.recipient)):
+            self._decode_pong(message_wrapper)
+        if (Runner.STATE_HELLO_SENT == self._state):
+            if (self._routing_id == message_wrapper.recipient):
+                self._decode_hello_reply(message_wrapper)
+        elif (Runner.STATE_WELCOME == self._state):
+            self._decode_game_state_init(message_wrapper)
+        elif (Runner.STATE_HELLO_SENT_READY == self._state):
+            self._decode_hello_reply_ready(message_wrapper)
+        elif (Runner.STATE_WAITING_GAME_START):
+            self._decode_game_state_start(message_wrapper)
+        elif (Runner.STATE_GAME_RUNNING):
+            self._decode_game_state_running(message_wrapper)
+
+    def _receive(self):
+        try:
+            message = self._subscribe_socket.recv(zmq.NOBLOCK)
+            if (message):
+                message_wrapper = MessageWrapper(message)
+                if (message_wrapper.recipient in
+                        ("all_clients", self._routing_id)):
+                    return message_wrapper
+        except zmq.Again:
+            pass
+        # except zmq.Again as e:
+            # logging.debug("no message: " + str(e))
+        return None
+
+    def _build_hello(self, ready):
+        pb_message = pb_controller.Hello()
+        name = "JAMBON"
+        pb_message.name = name
+        pb_message.ready = ready
+        payload = pb_message.SerializeToString()
+        return self._routing_id + ' Hello ' + payload
+
+    def _decode_pong(self, message_wrapper):
+        logging.debug("_decode_pong")
+        message = pb_robot.Pong()
+        message.ParseFromString(message_wrapper.payload)
+        logging.info(
+                "Pong ; len(timing) = " + str(len(message.timing)))
+        global NAME
+        for timing in message.timing:
+            timestamp = timing.timestamp
+            if (NAME == timing.logger):
+                timestamp_now = int(round(time.time() * 1000))
+                logging.info("timestamp_now = " + str(timestamp_now))
+                logging.info("timestamp = " + str(timestamp))
+                elapsed = timestamp_now - timestamp
+            else:
+                elapsed = timing.elapsed
+            logging.info("'{logger}': @{timestamp} for {elapsed}".format(
+                logger=timing.logger,
+                timestamp=timestamp,
+                elapsed=elapsed))
+
+    def _decode_hello_reply(self, message_wrapper):
+        logging.debug("_decode_hello_reply " +
+                      str(message_wrapper.message_type))
+        if ("Welcome" == message_wrapper.message_type):
+            self._handle_welcome(message_wrapper.payload)
+        elif ("Goodbye" == message_wrapper.message_type):
+            self._handle_goodbye(message_wrapper.payload)
+        else:
+            logging.debug("Wrong message type: " +
+                          message_wrapper.message_type)
+
+    def _decode_hello_reply_ready(self, message_wrapper):
+        if ("Welcome" == message_wrapper.message_type):
+            self._handle_welcome_ready(message_wrapper.payload)
+        elif ("Goodbye" == message_wrapper.message_type):
+            self._handle_goodbye(message_wrapper.payload)
+        else:
+            logging.debug("Wrong message type: " +
+                          message_wrapper.message_type)
+
+    def _handle_welcome(self, payload):
+        message = pb_server_game.Welcome()
+        message.ParseFromString(payload)
+        logging.info(
+                "Welcome ; id = " + str(message.id) +
+                " ; robot = '" + message.robot + "'" +
+                " ; team = '" + message.team + "'")
+        self._robot = message.robot
+        self._team = message.team
+        self._routing_id = str(message.id)
+        self._state = Runner.STATE_WELCOME
+        if (message.game_state):
+            self._check_start_game(message.game_state)
+
+    def _handle_welcome_ready(self, payload):
+        message = pb_server_game.Welcome()
+        message.ParseFromString(payload)
+        logging.info(
+                "Welcome [ready] ; id = " + str(message.id) +
+                " ; robot = '" + message.robot + "'" +
+                " ; team = '" + message.team + "'")
+        self._robot = message.robot
+        self._team = message.team
+        self._routing_id = str(message.id)
+        if (message.game_state):
+            self._check_start_game(message.game_state)
+        else:
+            self._state = Runner.STATE_WAITING_GAME_START
+
+    def _configure(self, game_state):
+        logging.info("playing ? " + str(game_state.playing))
+        logging.info("time left: " + str(game_state.seconds))
+        # self._update_running(game_state.playing)
+        for team in game_state.teams:
+            logging.info(team.name + " (" + str(team.num_players) +
+                         ") -> " + str(team.score))
+        # let's assume we configure the different visualisations now
+        self._push_socket.send(self._build_hello(True))
+        self._state = Runner.STATE_HELLO_SENT_READY
+
+    def _check_start_game(self, game_state):
+        logging.info("_check_start_game: " + str(game_state.playing))
+        if (game_state.playing):
+            self._state = Runner.STATE_GAME_RUNNING
+        else:
+            self._state = Runner.STATE_WAITING_GAME_START
+
+    def _decode_game_state_init(self, message_wrapper):
+        if ("GameState" == message_wrapper.message_type):
+            message = pb_server_game.GameState()
+            message.ParseFromString(message_wrapper.payload)
+            self._configure(message)
+
+    def _decode_game_state_start(self, message_wrapper):
+        if ("GameState" == message_wrapper.message_type):
+            message = pb_server_game.GameState()
+            message.ParseFromString(message_wrapper.payload)
+            self._check_start_game(message)
+
+    def _decode_game_state_running(self, message_wrapper):
+        if ("GameState" == message_wrapper.message_type):
+            message = pb_server_game.GameState()
+            message.ParseFromString(message_wrapper.payload)
+            self._update_visualisations(message)
+
+    def _update_visualisations(self, game_state):
+        logging.info("Updating visualisations")
+        if (not game_state.running):
+            self._state = Runner.STATE_WAITING_GAME_START
+
+    def _handle_goodbye(self, payload):
+        message = pb_server_game.Goodbye()
+        message.ParseFromString(payload)
+        logging.info("Goodbye ...")
+        self._abort = True
+
+    def send_input(self, joystick, force_ping):
+        if (joystick.has_new_values):
+            pb_input = pb_controller.Input()
+            pb_input.move.left = joystick.left
+            pb_input.move.right = joystick.right
+            pb_input.fire.weapon1 = joystick.fire_weapon1
+            pb_input.fire.weapon2 = joystick.fire_weapon2
+            payload = pb_input.SerializeToString()
+            message = self._routing_id + ' Input ' + payload
+            logging.debug("message sent: " + repr(message))
+            self._push_socket.send(message)
+            if (joystick.ping or force_ping):
+                pb_ping = pb_controller.Ping()
+                timing_event = pb_ping.timing.add()
+                global NAME
+                timing_event.logger = NAME
+                timestamp = int(round(time.time() * 1000))
+                timing_event.timestamp = timestamp
+                payload = pb_ping.SerializeToString()
+                message = self._routing_id + ' Ping ' + payload
+                logging.info("message sent: " + repr(message))
+                self._push_socket.send(message)
